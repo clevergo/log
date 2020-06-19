ae44a598f039cc3a08a19f362666eed7608f40e4
razonyang <razonyang@gmail.com>
2020-06-19 14:54:31 +0800

    Add StdLogger

--- logger.go
+++ logger.go
@@ -4,6 +4,10 @@
 
 package log
 
+import (
+	"log"
+)
+
 // Logger is a general logger interface.
 type Logger interface {
 	Debug(args ...interface{})
@@ -20,4 +24,65 @@ type Logger interface {
 
 	Fatal(args ...interface{})
 	Fatalf(format string, args ...interface{})
+}
+
+var _ Logger = New()
+
+// StdLogger is a wrapper of standard logger.
+type StdLogger struct {
+}
+
+// New returns a standard logger.
+func New() *StdLogger {
+	return &StdLogger{}
+}
+
+// Debug implements Logger's Debug.
+func (l *StdLogger) Debug(args ...interface{}) {
+	log.Print(args...)
+}
+
+// Debugf implements Logger's Debugf.
+func (l *StdLogger) Debugf(format string, args ...interface{}) {
+	log.Printf(format, args...)
+}
+
+// Info implements Logger's Info.
+func (l *StdLogger) Info(args ...interface{}) {
+	log.Print(args...)
+}
+
+// Infof implements Logger's Infof.
+func (l *StdLogger) Infof(format string, args ...interface{}) {
+	log.Printf(format, args...)
+}
+
+// Warn implements Logger's Warn.
+func (l *StdLogger) Warn(args ...interface{}) {
+	log.Print(args...)
+}
+
+// Warnf implements Logger's Warnf.
+func (l *StdLogger) Warnf(format string, args ...interface{}) {
+	log.Printf(format, args...)
+}
+
+// Error implements Logger's Error.
+func (l *StdLogger) Error(args ...interface{}) {
+	log.Print(args...)
+}
+
+// Errorf implements Logger's Errorf.
+func (l *StdLogger) Errorf(format string, args ...interface{}) {
+	log.Printf(format, args...)
+}
+
+// Fatal implements Logger's Fatal.
+func (l *StdLogger) Fatal(args ...interface{}) {
+	log.Fatal(args...)
+}
+
+// Fatalf implements Logger's Fatalf.
+func (l *StdLogger) Fatalf(format string, args ...interface{}) {
+	log.Fatalf(format, args...)
 }
--- logger_test.go
+++ logger_test.go
@@ -3,3 +3,54 @@
 // in the LICENSE file.
 
 package log
+
+import (
+	"bytes"
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStdLogger(t *testing.T) {
+	cases := []struct {
+		method string
+		format string
+		args   []interface{}
+		out    string
+	}{
+		{"debug", "", []interface{}{"debug"}, "debug"},
+		{"debugf", "msg: %s", []interface{}{"debugf"}, "msg: debugf"},
+		{"info", "", []interface{}{"info"}, "info"},
+		{"infof", "msg: %s", []interface{}{"infof"}, "msg: infof"},
+		{"warn", "", []interface{}{"warn"}, "warn"},
+		{"warnf", "msg: %s", []interface{}{"warnf"}, "msg: warnf"},
+		{"error", "", []interface{}{"error"}, "error"},
+		{"errorf", "msg: %s", []interface{}{"errorf"}, "msg: errorf"},
+	}
+	logger := New()
+	out := &bytes.Buffer{}
+	log.SetOutput(out)
+	for _, test := range cases {
+		out.Reset()
+		switch test.method {
+		case "debug":
+			logger.Debug(test.args...)
+		case "debugf":
+			logger.Debugf(test.format, test.args...)
+		case "info":
+			logger.Info(test.args...)
+		case "infof":
+			logger.Infof(test.format, test.args...)
+		case "warn":
+			logger.Warn(test.args...)
+		case "warnf":
+			logger.Warnf(test.format, test.args...)
+		case "error":
+			logger.Error(test.args...)
+		case "errorf":
+			logger.Errorf(test.format, test.args...)
+		}
+		assert.Contains(t, out.String(), test.out)
+	}
+}
